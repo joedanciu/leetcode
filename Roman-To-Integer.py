c50886ccc148c4a83ffcb587d1f10aa9c8cd8b54
Joseph Danciu <jd2279@nau.edu>
2022-08-18 03:22:08 -0500

    accepted answer

--- Roman-To-Integer.py
+++ Roman-To-Integer.py
@@ -6,23 +6,11 @@ class Solution(object):
         """
         romanNum = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
         total = 0
-        for i in range(len(s)):
+        for i in range(0, len(s)-1):
             if romanNum[s[i]] < romanNum[s[i+1]]:
-                total = total + (romanNum[s[i+1]]-romanNum[s[i]])
+                total = total - romanNum[s[i]]
+                i = i + 2
             else: 
                 total = total + romanNum[s[i]]
-        return total
-
-'''romanNum = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-        total = 0
-        for i in range(len(s)):
-            for j in range(i + 1, len(s)):
-                if romanNum[s[i]] < romanNum[s[j]]:
-                    beingAdded = (romanNum[s[i+1]]-romanNum[s[i]])
-                    total = total + beingAdded
-                    print("Detected " + str(romanNum[s[i+1]]) + "minus" + str(romanNum[s[i]]) + "so adding" + str(beingAdded))
-                else: 
-                    beingAdded = romanNum[s[i]]
-                    total = total + beingAdded
-                    print("Detected " + str(romanNum[s[i]]) +  "so adding" + str(beingAdded))
-        return total'''
+                i = i + 1
+        return total + romanNum[s[-1]]
