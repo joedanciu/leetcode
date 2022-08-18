e40ccb8515fb60ee03258fa21998cc955b10476e
Joseph Danciu <jd2279@nau.edu>
2022-08-18 13:53:34 -0500

    Delete twoSum.py

--- twoSum.py
+++
@@ -1,8 +0,0 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        values = {}
-        for idx, value in enumerate(nums):
-            if target - value in values:
-                return [values[target - value], idx]
-            else:
-                values[value] = idx
